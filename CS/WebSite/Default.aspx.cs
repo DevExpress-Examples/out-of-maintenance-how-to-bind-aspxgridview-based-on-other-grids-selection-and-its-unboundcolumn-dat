f1132683c9b3fdb9b08cab7e74dad4c2a9c19bd1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Collections.Generic;
+using DevExpress.Web.ASPxGridView;
+using DevExpress.Web.ASPxEditors;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Init(object sender, EventArgs e) {
+        if (Session["dataSource"] == null)
+            Session["dataSource"] = GetDataSource();
+        ASPxGridView1.DataSource = Session["dataSource"];
+        ASPxGridView1.DataBind();
+
+        if (Session["dataSourceSelectedValues"] != null) {
+            ASPxGridView2.DataSource = Session["dataSourceSelectedValues"];
+            ASPxGridView2.DataBind();
+        }
+    }
+    protected void txtLB_Init(object sender, EventArgs e) {
+        ASPxTextBox textBox = (ASPxTextBox)sender;
+        GridViewDataItemTemplateContainer templateContainer = (GridViewDataItemTemplateContainer)textBox.NamingContainer;
+        textBox.ClientInstanceName = string.Format("textBox_{0}", templateContainer.VisibleIndex);
+        textBox.ClientSideEvents.TextChanged = string.Format("function(s, e) {{ OnTextChanged(s, e, {0}); }}", templateContainer.VisibleIndex);
+
+        Dictionary<object, int> lowerBoundStorage = Session["lowerBoundStorage"] as Dictionary<object, int>;
+        if (lowerBoundStorage != null) {
+            object key = templateContainer.KeyValue;
+            if (lowerBoundStorage.ContainsKey(key))
+                textBox.Value = lowerBoundStorage[key];
+        }
+    }
+    protected void ASPxGridView1_CustomUnboundColumnData(object sender, DevExpress.Web.ASPxGridView.ASPxGridViewColumnDataEventArgs e) {
+        if (e.Column.FieldName == "LowerBound") {
+            Dictionary<object, int> lowerBoundStorage = Session["lowerBoundStorage"] as Dictionary<object, int>;
+            if (lowerBoundStorage == null) {
+                lowerBoundStorage = new Dictionary<object, int>();
+                Session["lowerBoundStorage"] = lowerBoundStorage;
+            }
+            object key = e.GetListSourceFieldValue(e.ListSourceRowIndex, "CategoryID");
+            if (lowerBoundStorage.ContainsKey(key))
+                e.Value = lowerBoundStorage[key];
+            else
+                e.Value = 0;
+        }
+    }
+    private DataTable GetDataSource() {
+        DataTable dataTable;
+
+        using (OleDbConnection connection = new OleDbConnection()) {
+            connection.ConnectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0}", MapPath("~/App_Data/nwind.mdb"));
+
+            dataTable = new DataTable();
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(string.Empty, connection);
+            adapter.SelectCommand.CommandText = "SELECT [CategoryID], [CategoryName], [Description] FROM [Categories]";
+            adapter.Fill(dataTable);
+        }
+
+        return dataTable;
+    }
+    protected void ASPxGridView2_CustomCallback(object sender, ASPxGridViewCustomCallbackEventArgs e) {
+        ASPxGridView g2 = (ASPxGridView)sender;
+        DataTable dataSourceSelectedValues = new DataTable();
+
+        dataSourceSelectedValues.Columns.Add(new DataColumn("CategoryID"));
+        dataSourceSelectedValues.Columns.Add(new DataColumn("CategoryName"));
+        dataSourceSelectedValues.Columns.Add(new DataColumn("Description"));
+        dataSourceSelectedValues.Columns.Add(new DataColumn("LowerBound"));
+
+        dataSourceSelectedValues.PrimaryKey = new DataColumn[] { dataSourceSelectedValues.Columns["CategoryID"] };
+        int indexSelectedValues = 0;
+
+        Dictionary<object, int> lowerBoundStorage = Session["lowerBoundStorage"] as Dictionary<object, int>;
+        if (lowerBoundStorage == null)
+            lowerBoundStorage = new Dictionary<object, int>();
+        for (int i = 0; i < ASPxGridView1.VisibleRowCount; i++) {
+            ASPxTextBox txtLowerBound = (ASPxTextBox)ASPxGridView1.FindRowCellTemplateControl(i, (GridViewDataColumn)ASPxGridView1.Columns["LowerBound"], "txtLB");
+            int lowerBound = 0;
+            object key = ASPxGridView1.GetRowValues(i, "CategoryID");
+            if (txtLowerBound != null) {
+                lowerBound = int.Parse(txtLowerBound.Text.Trim());
+                if (!lowerBoundStorage.ContainsKey(key))
+                    lowerBoundStorage.Add(key, lowerBound);
+                else
+                    lowerBoundStorage[key] = lowerBound;
+            } else {
+                if (lowerBoundStorage.ContainsKey(key))
+                    lowerBound = lowerBoundStorage[key];
+            }
+            
+            if (ASPxGridView1.Selection.IsRowSelected(i)) {
+                dataSourceSelectedValues.ImportRow(ASPxGridView1.GetDataRow(i));
+                DataRow dataRow = dataSourceSelectedValues.Rows[indexSelectedValues];
+                dataRow["LowerBound"] = lowerBound;
+                indexSelectedValues++;
+            }
+        }
+        Session["dataSourceSelectedValues"] = dataSourceSelectedValues;
+        Session["lowerBoundStorage"] = lowerBoundStorage;
+
+        g2.DataSource = Session["dataSourceSelectedValues"];
+        g2.DataBind();
+    }
+    protected void ASPxCallback1_Callback(object source, DevExpress.Web.ASPxCallback.CallbackEventArgs e) {
+        string[] parameters = e.Parameter.Split('|');
+        int visibleIndex = int.Parse(parameters[0]);
+        int lowerBound = int.Parse(parameters[1]);
+
+        Dictionary<object, int> lowerBoundStorage = Session["lowerBoundStorage"] as Dictionary<object, int>;
+        if (lowerBoundStorage == null)
+            lowerBoundStorage = new Dictionary<object, int>();
+
+        object key = ASPxGridView1.GetRowValues(visibleIndex, "CategoryID");
+        if (!lowerBoundStorage.ContainsKey(key))
+            lowerBoundStorage.Add(key, lowerBound);
+        else
+            lowerBoundStorage[key] = lowerBound;
+    }
+}
---
+++ VB/WebSite/Default.aspx
@@ -0,0 +1,76 @@
+<%@ Page Language="vb" AutoEventWireup="true" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+
+<%@ Register Assembly="DevExpress.Web.v9.3, Version=9.3.5.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
+	Namespace="DevExpress.Web.ASPxCallback" TagPrefix="dx" %>
+<%@ Register Assembly="DevExpress.Web.v9.3, Version=9.3.5.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
+	Namespace="DevExpress.Web.ASPxHiddenField" TagPrefix="dx" %>
+<%@ Register Assembly="DevExpress.Web.ASPxGridView.v9.3, Version=9.3.5.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
+	Namespace="DevExpress.Web.ASPxGridView" TagPrefix="dxwgv" %>
+<%@ Register Assembly="DevExpress.Web.ASPxEditors.v9.3, Version=9.3.5.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
+	Namespace="DevExpress.Web.ASPxEditors" TagPrefix="dxe" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title></title>
+	<script type="text/javascript" language="javascript">
+		function OnTextChanged(s, e, visibleIndex) {
+			callback.PerformCallback(visibleIndex + '|' + s.GetValue().toString())
+		}
+
+		function OnCallbackComplete(s, e) {
+			grid2.PerformCallback('');
+		}
+	</script>
+</head>
+<body>
+	<form id="form1" runat="server">
+	<div>
+		<dx:ASPxCallback ID="ASPxCallback1" runat="server" 
+			ClientInstanceName="callback" OnCallback="ASPxCallback1_Callback">
+			<ClientSideEvents CallbackComplete="OnCallbackComplete" />
+		</dx:ASPxCallback>
+		<dxwgv:ASPxGridView ID="ASPxGridView1" runat="server" KeyFieldName="CategoryID" ClientInstanceName="grid1"
+			OnCustomUnboundColumnData="ASPxGridView1_CustomUnboundColumnData">
+			<Columns>
+				<dxwgv:GridViewCommandColumn ShowSelectCheckbox="True" VisibleIndex="0">
+				</dxwgv:GridViewCommandColumn>
+				<dxwgv:GridViewDataTextColumn FieldName="CategoryID" VisibleIndex="1">
+				</dxwgv:GridViewDataTextColumn>
+				<dxwgv:GridViewDataTextColumn FieldName="CategoryName" VisibleIndex="2">
+				</dxwgv:GridViewDataTextColumn>
+				<dxwgv:GridViewDataTextColumn FieldName="Description" VisibleIndex="3">
+				</dxwgv:GridViewDataTextColumn>
+				<dxwgv:GridViewDataTextColumn FieldName="LowerBound" VisibleIndex="4" UnboundType="Integer">
+					<DataItemTemplate>
+						<dxe:ASPxTextBox ID="txtLB" runat="server" Text="0" Width="60px" OnInit="txtLB_Init">
+						</dxe:ASPxTextBox>
+					</DataItemTemplate>
+				</dxwgv:GridViewDataTextColumn>
+			</Columns>
+			<ClientSideEvents SelectionChanged="function(s, e) {
+				if (!callback.InCallback())
+					grid2.PerformCallback('');
+			}" />
+			<SettingsBehavior AllowMultiSelection="true" />
+			<SettingsPager PageSize="5"></SettingsPager>
+		</dxwgv:ASPxGridView>
+		<br />
+		<dxwgv:ASPxGridView ID="ASPxGridView2" runat="server" KeyFieldName="CategoryID" ClientInstanceName="grid2"
+			OnCustomCallback="ASPxGridView2_CustomCallback">
+			<Columns>
+				<dxwgv:GridViewCommandColumn ShowSelectCheckbox="True" VisibleIndex="0">
+				</dxwgv:GridViewCommandColumn>
+				<dxwgv:GridViewDataTextColumn FieldName="CategoryID" VisibleIndex="1">
+				</dxwgv:GridViewDataTextColumn>
+				<dxwgv:GridViewDataTextColumn FieldName="CategoryName" VisibleIndex="2">
+				</dxwgv:GridViewDataTextColumn>
+				<dxwgv:GridViewDataTextColumn FieldName="Description" VisibleIndex="3">
+				</dxwgv:GridViewDataTextColumn>
+				<dxwgv:GridViewDataTextColumn FieldName="LowerBound" VisibleIndex="4">
+				</dxwgv:GridViewDataTextColumn>
+			</Columns>
+		</dxwgv:ASPxGridView>
+	</div>
+	</form>
+</body>
+</html>
